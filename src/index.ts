7e2dd00dad75c8e17913bc4b4ab34cd84a506422
Verity <joshua-schmidt@online.de>
2021-02-23 10:20:28 +0100

    :sparkles: Add first window

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello world!")
+const { app, BrowserWindow } = require("electron")
+
+function createWindow() {
+
+    const window = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    window.loadFile("D:\\Development\\Workspace\\TypeScript\\spotify-mgk-rate\\src\\frontend\\index.html")
+
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
